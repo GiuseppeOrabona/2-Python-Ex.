59216f98c9f032908ef1dabf2a10ca544bc43719
Giuseppe Orabona <35228402+GiuseppeOrabona@users.noreply.github.com>
2018-01-15 19:26:24 +0100

    Add files via upload

---
+++ LenParola.py
@@ -0,0 +1,7 @@
+#Introduzione capitolo STRINGHE Python 3.5.2
+
+#Esercizio semplice che calcola la lunghezza di una parola inserita
+
+
+Parola=str(input("Inserire una parola cui si vuole conoscere la lunghezza:"))
+print(len(Parola))
