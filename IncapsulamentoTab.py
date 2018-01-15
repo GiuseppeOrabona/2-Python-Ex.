f7306e523f581a70f7bae161f64456e0ed8215cf
Giuseppe Orabona <35228402+GiuseppeOrabona@users.noreply.github.com>
2018-01-15 18:48:44 +0100

    Add files via upload

---
+++ IncapsulamentoTab.py
@@ -0,0 +1,24 @@
+def StampaMultipli(n):
+    i = 1
+    while i <= 6:
+        print (i*n, end="   ")
+        i = i + 1
+        print 
+        
+
+i = 1
+while i <= 6:
+    StampaMultipli(i)
+    i = i + 1
+    print (i)
+
+
+
+#Vi è purtroppo un errore nella stampa che non sono riuscito a risolvere.
+#Compare un ulteriore colonna (l'ultima) che è superflua e non è appartenente
+#all'exe del programma
+#Se avete correzioni o soluzioni aggiungetele, ne sarei grato.
+        
+            
+   
+
