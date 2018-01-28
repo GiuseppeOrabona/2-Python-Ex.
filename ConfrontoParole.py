8ae4fb2d6bc0755c0d8fa6585b859647617122fb
Giuseppe Orabona <35228402+GiuseppeOrabona@users.noreply.github.com>
2018-01-28 12:23:47 +0100

    Add files via upload

---
+++ ConfrontoParole.py
@@ -0,0 +1,18 @@
+#Confronto tra stringhe
+
+Parola = str(input("Inserisci un frutto:"))
+
+
+if Parola == "BANANA":
+    print("Stai parlando di un frutto")
+
+if Parola < "BANANA":
+
+    print("La tua parola" " "+ Parola +" " "viene prima di BANANA.")
+elif Parola > "BANANA":
+    print("La tua parola"  " "+ Parola +" " "viene dopo BANANA.")
+else :
+    print("Hai inserito la parola BANANA")
+
+
+
